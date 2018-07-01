c4a30d51184f39bf3ddd3e31a3f2c8791fa994ea
Caleb Froese <caleb.froese@gmail.com>
2018-07-01 21:33:04 +0930

    Add hydration from localstorage

--- src/actions/app.actions.ts
+++ src/actions/app.actions.ts
@@ -1,11 +1,16 @@
 import { Action } from '@ngrx/store';
 
 export enum AppActions {
+  Hydrate = '[App] Hydrate',
   Initialize = '[App] Initialize',
 }
 
+export class Hydrate implements Action {
+  readonly type = AppActions.Hydrate;
+  constructor(public payload: any) {}
+}
 export class Initialize implements Action {
   readonly type = AppActions.Initialize;
 }
 
-export type AppActionsUnion = Initialize;
+export type AppActionsUnion = Hydrate | Initialize;
--- src/effects/app.effects.ts
+++ src/effects/app.effects.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { Actions, Effect } from '@ngrx/effects';
-import { map } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { map, switchMap } from 'rxjs/operators';
 
-import { AppActions } from '../actions/app.actions';
+import { AppActions, Hydrate } from '../actions/app.actions';
+import { reducers, State } from '../reducers';
+import * as fromApp from '../reducers/app.reducer';
+import * as fromClient from '../reducers/client.reducer';
+import * as fromJob from '../reducers/job.reducer';
 
 @Injectable()
 export class AppEffects {
@@ -13,5 +19,33 @@ export class AppEffects {
     }),
   );
 
-  constructor(private actions$: Actions) {}
+  @Effect()
+  loadFromStorage$ = this.actions$.ofType(AppActions.Initialize).pipe(
+    switchMap(() => {
+      return fromPromise(
+        Promise.all(
+          Object.getOwnPropertyNames(reducers).map(slice =>
+            this.storage.get(slice).then(data => ({ slice, data })),
+          ),
+        ),
+      );
+    }),
+    map(data =>
+      data.reduce(
+        (obj: any, { slice, data }: any) =>
+          data ? { ...obj, [slice]: data } : obj,
+        {},
+      ),
+    ),
+    map(data => {
+      const state: State = {
+        app: fromApp.initialState,
+        client: fromClient.initialState,
+        job: fromJob.initialState,
+      };
+      return new Hydrate(state);
+    }),
+  );
+
+  constructor(public actions$: Actions, public storage: Storage) {}
 }
